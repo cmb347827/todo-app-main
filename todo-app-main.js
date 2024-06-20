b2acd1745663cdd9a11a36f884332e2c4031013b
cmb347827 <v38d422ll621dem@gmail.com>
2024-06-20 00:32:29 -0400

    More dark scss

--- todo-app-main.js
+++ todo-app-main.js
@@ -231,7 +231,7 @@ const updateTaskContainer = (data) => {
                           <label class='visually-hidden'>Check or uncheck task</label>
                           <input onchange='setRemoveChecked(event)' class="form-check-input" type="checkbox" ${which} >
                           <textarea onchange='updateTask(event)' class="form-control">${task}</textarea>
-                          <button onclick='deleteTask(event)' type='button' class='delete-task btn'><svg class='cross' xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg></button>
+                          <button onclick='deleteTask(event)' type='button' class='delete-task btn'><svg  class='cross' xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg></button>
                         </div>
                         <hr class='bottom-hr'>
                  `)
