5c31c0ebb1ef99879f4c2f4a22e734f04f4a3261
cmb347827 <117379227+cmb347827@users.noreply.github.com>
2025-01-30 10:26:22 -0500

    Update todo-app-main.js

--- todo-app-main.js
+++ todo-app-main.js
@@ -321,7 +321,7 @@ const updateTaskContainer = (data) => {
                           <input onchange='setRemoveChecked(event)' class="form-check-input checkbox-round" type="checkbox" ${which} >
                           <label class='visually-hidden'>Check or uncheck task</label>
                           <textarea style='${whichStyle}' onchange='updateTask(event)' class="form-control">${task}</textarea>
-                          <button onclick='deleteTask(event)' type='button' class='delete-task btn'><svg  class='cross' xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg></button>
+                          <button title='delete task' onclick='deleteTask(event)' type='button' class='delete-task btn'><svg  class='cross' xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg></button>
                         </div>
                         <hr class='bottom-hr'>
                  `)
